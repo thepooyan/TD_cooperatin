fe047f18e1868ffb818fb1d0356f26716c9e00a3
pooyan <thepooyan@gmail.com>
2023-07-13 15:40:54 +0330

    teachers slider done

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,37 @@
-const cooperationForm = dc.query('form#cooperation');
+$(function () {
 
-if (cooperationForm) {
-    cooperationForm.onsubmit = e => {
-        e.preventDefault();
-        alert('submit');
+    const cooperationForm = dc.query('form#cooperation');
+    if (cooperationForm) {
+        cooperationForm.onsubmit = e => {
+            e.preventDefault();
+            alert('submit');
+        }
     }
-}
 
+    //group checkbox
+    dc.queries('input[type="checkbox"][data-groupWith]').forEach(item => {
+        const groupName = item.dataset.groupwith;
+        const group = dc.queries(`[data-groupWith="${groupName}"]`);
+        if (!group) return
+        item.onchange = () => {
+            group.forEach(i => i.checked = false);
+            item.checked = true;
+        }
+    })
 
-//group checkbox
-dc.queries('input[type="checkbox"][data-groupWith]').forEach(item=>{
-    const groupName = item.dataset.groupwith;
-    const group = dc.queries(`[data-groupWith="${groupName}"]`);
-    if (!group) return
-    item.onchange = () => {
-        group.forEach(i => i.checked  = false );
-        item.checked = true;
+    const teacherCarousel = dc.query('body > .teachers.owl-carousel');
+    if (teacherCarousel) {
+        $(teacherCarousel).owlCarousel({
+            autoWidth: false,
+            rtl: true,
+            nav: true,
+            items: 1,
+            loop: true,
+            autoplay: true,
+            navText: '',
+            autoplayTimeout: 3200,
+            autoplayHoverPause: true,
+            autoplaySpeed: 900,
+        });
     }
 })

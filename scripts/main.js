ef22bd3f9b05e82542f0337d6c358a64e9302ef8
pooyan <thepooyan@gmail.com>
2023-07-13 11:46:49 +0330

    checkbox things done

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
+const cooperationForm = dc.query('form#cooperation');
 
+if (cooperationForm) {
+    cooperationForm.onsubmit = e => {
+        e.preventDefault();
+        alert('submit');
+    }
+}
+
+
+//group checkbox
+dc.queries('input[type="checkbox"][data-groupWith]').forEach(item=>{
+    const groupName = item.dataset.groupwith;
+    const group = dc.queries(`[data-groupWith="${groupName}"]`);
+    if (!group) return
+    item.onchange = () => {
+        group.forEach(i => i.checked  = false );
+        item.checked = true;
+    }
+})
